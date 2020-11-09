3196d04cabcd1894882a4dca60cddd7f8d9b9b2b
Promlin <somebody2605@yandex.ru>
2020-11-09 13:13:00 +0300

    начало начал

---
+++ Notebook/Notebook/IPersonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    public interface IPersonInfo
+    {
+        void AddPerson();
+        void ShowAllInfo(PersonInfo personInfo);
+        void ShowShortInfo(PersonInfo personInfo);
+        void CorrectInfo();
+        void DeleteInfo();
+        void ShowAllPersons();
+    }
+}
---
+++ Notebook/Notebook/PersonInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    public class PersonInfo
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public string Notes { get; set; }  
+
+        public PersonInfo(string surname, string name, string phone)
+        {
+            Surname = surname;
+            Name = name;
+            Phone = phone;
+        }
+    }
+}
